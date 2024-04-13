ecdf9b375ddd87e3199a1cfe841562412e8bc78f
Vanessa Bertoldo <126096822+Vanessa-Bertoldo@users.noreply.github.com>
2024-04-13 20:34:39 -0300

    project base

--- CleanArchMvc/CleanArchMvc.Domain/obj/Debug/net8.0/CleanArchMvc.Domain.AssemblyInfo.cs
+++ CleanArchMvc/CleanArchMvc.Domain/obj/Debug/net8.0/CleanArchMvc.Domain.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("CleanArchMvc.Application")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("CleanArchMvc.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc5a673d4f5fe1d5e2223b2d0be9d524d03bb48e")]
-[assembly: System.Reflection.AssemblyProductAttribute("CleanArchMvc.Application")]
-[assembly: System.Reflection.AssemblyTitleAttribute("CleanArchMvc.Application")]
+[assembly: System.Reflection.AssemblyProductAttribute("CleanArchMvc.Domain")]
+[assembly: System.Reflection.AssemblyTitleAttribute("CleanArchMvc.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
