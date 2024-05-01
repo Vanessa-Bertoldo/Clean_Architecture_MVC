a62e730e5cfd3209ca995a1e39f826863e737b3b
Vanessa Bertoldo <126096822+Vanessa-Bertoldo@users.noreply.github.com>
2024-04-30 22:34:33 -0300

    nsert class product configuration

---
+++ CleanArchMvc/CleanArchMvc.Infra.Data/EntitiesConfiguration/Product Configration.cs
@@ -0,0 +1,24 @@
+﻿using CleanArchMvc.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.Infra.Data.EntitiesConfiguration
+{
+    public class Product_Configration : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Name).HasMaxLength(100).IsRequired();
+            builder.Property(x => x.Description).HasMaxLength(200).IsRequired();
+            builder.Property(x => x.Price).HasPrecision(10,2).IsRequired();
+            //configuração de relacionamento um para muitos
+            builder.HasOne(x => x.Category).WithMany(e => e.Products).HasForeignKey(x => x.CategoryId);
+        }
+    }
+}
--- CleanArchMvc/CleanArchMvc.Infra.Data/obj/Debug/net8.0/CleanArchMvc.Infra.Data.AssemblyInfo.cs
+++ CleanArchMvc/CleanArchMvc.Infra.Data/obj/Debug/net8.0/CleanArchMvc.Infra.Data.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CleanArchMvc.Infra.Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e28fbec3cc49b652c6d8d02ae0e730f993f4de77")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ebbd18d0465f9022cad9b0c150a2141c3a377a37")]
 [assembly: System.Reflection.AssemblyProductAttribute("CleanArchMvc.Infra.Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CleanArchMvc.Infra.Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
